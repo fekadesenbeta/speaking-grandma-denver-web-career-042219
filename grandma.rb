c62fdb57acb2eb56d270515b20ef05549f914ae5
Fekade Senbeta <fekadesenbeta@gmail.com>
2019-04-19 22:51:43 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,7 +14,10 @@
 
 def speak_to_grandma(phrase)
   if phrase = .downcase
-    puts "HUH?! SPEAK UP, SONNY!"
+    return "HUH?! SPEAK UP, SONNY!"
+  elsif phrase = .upcase 
+    return "NO, NOT SINCE 1938!"
+  else phrase = 
   
   
 end 
