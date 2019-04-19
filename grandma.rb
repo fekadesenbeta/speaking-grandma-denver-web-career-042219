bc99a213c8a3c6359f9cf9f9f019deba4d560744
Fekade Senbeta <fekadesenbeta@gmail.com>
2019-04-19 22:56:48 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,11 +13,11 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-  if phrase = "phrase".downcase
+  if phrase = phrase.downcase
     return "HUH?! SPEAK UP, SONNY!"
-  elsif phrase = "phrase".upcase 
+  elsif phrase = phrase.upcase 
     return "NO, NOT SINCE 1938!"
   else phrase = "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN"
-end 
+ end 
 end
